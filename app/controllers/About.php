fdb15412a6ba5ba8f5c4c3d0f7170598604614a6
riyanardyanto <riyanardyanto@gmail.com>
2022-12-15 21:33:21 +0700

    first commit

---
+++ app/controllers/About.php
@@ -0,0 +1,26 @@
+<?php
+
+class About extends controller{
+    public function index($nama='Riyan', $pekerjaan='mahasiswa', $umur=30)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan']=$pekerjaan;
+        $data['umur']=$umur;
+        $data['judul']='About Me';
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+        
+    }
+
+
+    public function page()
+    {
+        $data['judul']='Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+       
+    }
+}
