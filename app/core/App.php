1cf10e79cefc33b3f579edd23175a3ea18cdae9f
riyanardyanto <riyanardyanto@gmail.com>
2022-12-16 18:03:55 +0700

    Format text by prettier

--- app/core/App.php
+++ app/core/App.php
@@ -1,42 +1,39 @@
 <?php
 
-class App {
+class App
+{
     protected $controller = 'Home';
     protected $method = 'index';
     protected $params = [];
 
 
     public function __construct()
-        {
+    {
         // controller
         $url = $this->parse_url();
-        
-        if($url == NULL)
-        {
-			$url = [$this->controller];
-		}
 
-        if( file_exists('../app/controllers/' .$url[0]. '.php')){
+        if ($url == NULL) {
+            $url = [$this->controller];
+        }
+
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
 
-        require_once '../app/controllers/' . $this->controller. '.php';
+        require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
         // method
-        if( isset($url[1])) 
-        {
-            if(method_exists($this->controller, $url[1]))
-            {
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
-                unset($url[1]) ;
+                unset($url[1]);
             }
         }
 
         // params
-        if(!empty($url))
-        {
+        if (!empty($url)) {
             $this->params = array_values($url);
         }
 
@@ -46,8 +43,7 @@ class App {
 
     public function parse_url()
     {
-        if(isset($_GET['url']))
-        {
+        if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/   ');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,8 +1,9 @@
 <?php
 
-class Controller {
-    public function view($view, $data=[])
+class Controller
+{
+    public function view($view, $data = [])
     {
-        require_once '../app/views/' .$view. '.php';
+        require_once '../app/views/' . $view . '.php';
     }
 }
