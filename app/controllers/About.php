95ed3cd281f026409a2cbd22d959ec6414a84b25
riyanardyanto <riyanardyanto@gmail.com>
2022-12-16 16:38:18 +0700

    Setting extension prettier

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,26 +1,25 @@
 <?php
 
-class About extends controller{
-    public function index($nama='Riyan', $pekerjaan='mahasiswa', $umur=30)
+class About extends controller
+{
+    public function index($nama = 'Riyan', $pekerjaan = 'mahasiswa', $umur = 30)
     {
         $data['nama'] = $nama;
-        $data['pekerjaan']=$pekerjaan;
-        $data['umur']=$umur;
-        $data['judul']='About Me';
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
 
         $this->view('templates/header', $data);
         $this->view('about/index', $data);
         $this->view('templates/footer');
-        
     }
 
 
     public function page()
     {
-        $data['judul']='Page';
+        $data['judul'] = 'Page';
         $this->view('templates/header', $data);
         $this->view('about/page');
         $this->view('templates/footer');
-       
     }
 }
