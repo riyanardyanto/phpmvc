d6dd14f1115504cc87ecd2f0281176031da8a6bf
riyanardyanto <riyanardyanto@gmail.com>
2022-12-17 13:45:31 +0700

    MVC insert data + modal box

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,5 +1,7 @@
 <?php
 
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
 class Mahasiswa_model
 {
     private $table = 'mahasiswa';
@@ -21,5 +23,22 @@ class Mahasiswa_model
         $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
         $this->db->bind('id', $id);
         return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa 
+                    VALUES 
+                    ('', :nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
     }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,13 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+
+            <br><br>
+
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -10,6 +17,58 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="nrp">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Akuntansi ">Akuntansi </option>
+                            <option value="Manajemen ">Manajemen </option>
+                            <option value="Psikologi ">Psikologi </option>
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Sistem Informasi">Sistem Informasi</option>
+                            <option value="Farmasi">Farmasi</option>
+                            <option value="Pendidikan Guru Sekolah Dasar">Pendidikan Guru Sekolah Dasar</option>
+                            <option value="Kedokteran">Kedokteran</option>
+                            <option value="PPKN">PPKN</option>
+                            <option value="Ilmu Hukum">Ilmu Hukum</option>
+                            <option value="Pendidikan Agama Islam">Pendidikan Agama Islam</option>
+                        </select>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
